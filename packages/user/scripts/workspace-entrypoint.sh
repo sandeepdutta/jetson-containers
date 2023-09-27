d89c9d36a808c50738e2d5ad803c0da3b70ba8f7
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-09-27 14:58:00 -0700

    added user

---
+++ packages/user/scripts/workspace-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+# Build ROS dependency
+echo "source /opt/ros/${ROS_DISTRO}/install/setup.bash" >> ~/.bashrc
+echo "alias ros2build='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo'" >> ~/.bashrc
+echo "export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda"
+source /opt/ros/${ROS_DISTRO}/install/setup.bash
+
+#sudo apt-get update
+#rosdep update
+
+# Restart udev daemon
+#sudo service udev restart
+
+$@
