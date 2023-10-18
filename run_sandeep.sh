beed5c61b7ff2f0ead9afbdae2f9f4a73bca1f69
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-10-18 09:44:16 -0700

    added run_sandeep.sh

--- run_sandeep.sh
+++ run_sandeep.sh
@@ -50,11 +50,26 @@ if [ $ARCH = "aarch64" ]; then
 		--volume /tmp/argus_socket:/tmp/argus_socket \
 		--volume /etc/enctune.conf:/etc/enctune.conf \
 		--volume /etc/nv_tegra_release:/etc/nv_tegra_release \
+		--privileged \
 		--volume /tmp/nv_jetson_model:/tmp/nv_jetson_model \
+		-v /home/sandeep/workspaces:/workspaces \
+    	-v /dev/*:/dev/* \
+    	-v /etc/localtime:/etc/localtime:ro \
+		-v /run/jtop.sock:/run/jtop.sock \
 		--volume $ROOT/data:/data \
 		--device /dev/snd \
 		--device /dev/bus/usb \
-		$DATA_VOLUME $DISPLAY_DEVICE $V4L2_DEVICES \
+		--user="admin" \
+		--entrypoint /usr/local/bin/scripts/workspace-entrypoint.sh \
+		--workdir /workspaces/isaac_ros-dev \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v $HOME/.Xauthority:/home/admin/.Xauthority:rw \
+		-e DISPLAY \
+		-e NVIDIA_VISIBLE_DEVICES=all \
+		-e NVIDIA_DRIVER_CAPABILITIES=all \
+		-e FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml \
+		-e ROS_DOMAIN_ID \
+		$DATA_VOLUME $V4L2_DEVICES \
 		"$@"
 
 elif [ $ARCH = "x86_64" ]; then
