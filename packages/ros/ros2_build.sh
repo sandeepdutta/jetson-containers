8d37a9e653756316d5b0abffc7f88b3ebd6f4d9e
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-10-18 14:52:19 -0700

    added jupyter_nemo and update ros2_build.sh

--- packages/ros/ros2_build.sh
+++ packages/ros/ros2_build.sh
@@ -143,7 +143,7 @@ rosdep install -y \
 # build it all - for verbose, see https://answers.ros.org/question/363112/how-to-see-compiler-invocation-in-colcon-build
 colcon build \
 	--merge-install \
-	--cmake-args -DCMAKE_BUILD_TYPE=Release 
+	--cmake-args -DCMAKE_BUILD_TYPE=Release --packages-ignore joy
     
 # remove build files
 rm -rf ${ROS_ROOT}/src
