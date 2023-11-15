51dddc94af0255f0313e74885dab00189ac36a40
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-11-14 18:03:18 -0800

    started whisper nemo

---
+++ build_whisper_neno.sh
@@ -0,0 +1,1 @@
+./build.sh --name=whisper_nemo --base=dustynv/nemo:r35.4.1 whisper user
--- run_whisper_nemo.sh
+++ run_whisper_nemo.sh
@@ -1,8 +1,8 @@
 # Re-use existing container.
-CONTAINER_NAME=jupyter_nemo
+CONTAINER_NAME=whisper_nemo
 if [ "$(docker ps -a --quiet --filter status=running --filter name=$CONTAINER_NAME)" ]; then
     echo "Attaching to running container: $CONTAINER_NAME"
     docker exec -i -t -u admin --workdir /workspaces/isaac_ros-dev $CONTAINER_NAME /bin/bash $@
     exit 0
 fi
-~/workspaces/jetson-containers/run_admin.sh   --name "$CONTAINER_NAME" --entrypoint "/usr/local/bin/scripts/jupyter_nemo-entrypoint.sh" jupyter_nemo:r35.4.1 $@
+~/workspaces/jetson-containers/run_admin.sh   --name "$CONTAINER_NAME" --entrypoint "/usr/local/bin/scripts/jupyter_nemo-entrypoint.sh" whisper_nemo:r35.4.1 $@
