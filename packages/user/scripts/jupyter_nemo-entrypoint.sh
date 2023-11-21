974edc06659d1fdf775d99b5f64efdd3e6618ecb
Sandeep Dutta <sandeep.dutta@ieee.org>
2023-11-21 13:48:16 -0800

    changed entry

--- packages/user/scripts/jupyter_nemo-entrypoint.sh
+++ packages/user/scripts/jupyter_nemo-entrypoint.sh
@@ -4,4 +4,5 @@ echo "allow 10 sec for JupyterLab to start @ http://$(hostname -I | cut -d' ' -f
 echo "JupterLab logging location:  /home/admin/jupyter.log  (inside the container)" 
 cat /home/admin/jupyter.log
 echo "export PYTHONPATH=/jetson-voice:/workspaces/microphone/usb_4_mic_array" >> /home/admin/.bashrc
+sed 's|torch\.distributed\.is_initialized|torch\.distributed\.is_available\(\) and torch.distributed.is_initialized|' -i /usr/local/lib/python3.8/dist-packages/nemo/collections/asr/modules/conv_asr.py
 /bin/bash
